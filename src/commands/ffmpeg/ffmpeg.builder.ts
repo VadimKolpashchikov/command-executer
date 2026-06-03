f97b972cb96c656caa3d09517ec18a47c9e05a12
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-06-03 20:53:02 +0300

    ffmpeg builder

---
+++ src/commands/ffmpeg/ffmpeg.builder.ts
@@ -0,0 +1,34 @@
+export class FfmpegBuilder {
+  private inputPath: string;
+  private options: Map<string, string> = new Map();
+
+  constructor() {
+    this.options.set('-c:v', 'libx264');
+  }
+
+  input(inputPath: string): this {
+    this.inputPath = inputPath;
+    return this;
+  }
+
+  setSize(width: number, height: number): this {
+    this.options.set('-s', `${width}x${height}`);
+    return this;
+  }
+
+  output(outputPath: string): string[] {
+    if (this.inputPath) {
+      throw new Error('Parameter input not found');
+    }
+
+    const args: string[] = ['-i', this.inputPath];
+
+    this.options.forEach((value, key) => {
+      args.push(key, value);
+    });
+
+    args.push(outputPath);
+
+    return args;
+  }
+}
